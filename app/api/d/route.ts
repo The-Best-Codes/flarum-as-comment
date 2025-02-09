9720ac98001ff98c9f0ea6f71493a2144774f929
The-Best-Codes <bestcodes.official@gmail.com>
2025-02-09 14:56:52 -0600

    fix typo and type errors

--- app/api/d/route.ts
+++ app/api/d/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force_dynamic";
+export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const searchParams = request.nextUrl.searchParams;
